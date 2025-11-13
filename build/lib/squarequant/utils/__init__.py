dfc47203d56d8d7ba114beca070b30ab8f3aa1c8
SquareQuant <contact@squarequant.org>
2025-11-13 01:46:47 +0100

    Delete build/lib/squarequant directory

--- build/lib/squarequant/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-# squarequant/utils/__init__.py
-"""
-Utility functions for the SquareQuant package
-"""
-
-# This file is intentionally left empty to mark the directory as a Python package
--- build/lib/squarequant/var/__init__.py
+++
@@ -1,6 +0,0 @@
-# squarequant/var/__init__.py
-"""
-Value-at-Risk components of the Squarequant package
-"""
-
-# This file is intentionally left empty to mark the directory as a Python package
